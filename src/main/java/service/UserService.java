35b4ced0fbb9858992c5e08bc4743f976f95d4f6
Ivan <shtrikh14@gmail.com>
2023-08-16 12:29:38 +0300

    commit

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,26 @@
+package service;
+
+
+import model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserService {
+    private final Map<Long, User> users = new HashMap<>();
+    private long nextUserId = 1;
+
+    public User createUser(String name, String email) {
+        User user = new User(nextUserId, name, email);
+        users.put(nextUserId, user);
+        nextUserId++;
+        return user;
+    }
+
+    public User getUserById(long userId) {
+        return users.get(userId);
+    }
+}
+
---
+++ src/test/java/com/example/cicd/UserServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.cicd;
+
+
+import model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class UserServiceTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserService userServiceUnderTest;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testCreateUser() {
+        when(userService.createUser("John Doe", "john@example.com")).thenReturn(new User(1, "John Doe", "john@example.com"));
+
+        User user = userServiceUnderTest.createUser("John Doe", "john@example.com");
+
+        assertEquals("John Doe", user.getName());
+        assertEquals("john@example.com", user.getEmail());
+    }
+
+    // Add more tests for other methods
+}
